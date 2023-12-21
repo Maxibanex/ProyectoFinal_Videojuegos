21dc2b65e2a28ac121e6cf90f2be0afa738b2574
Maxibanex <114372559+Maxibanex@users.noreply.github.com>
2023-12-21 01:20:37 -0400

    Jump Agregado
    Commit con Jump Agregado

--- Final Project Endless/Assets/Scripts/Move.cs
+++ Final Project Endless/Assets/Scripts/Move.cs
@@ -6,10 +6,18 @@ public class Move : MonoBehaviour
 {
     float Horizontal;
     public float MaxLeft,MaxRight;
-    public float MaxBack,MaxFor;
+    //public float MaxBack,MaxFor;
+    public AnimationCurve jumpCurve;
+
+    private bool Jumping = false;
+    public float JumpScale = 5f;
+    public float JumpDuration = 1f;
     public float Speed;
+    
     internal Transform tr;
     float yOriginal;
+    float yOffset;
+    
     private void Awake()
     {
         tr = transform;
@@ -26,7 +34,27 @@ public class Move : MonoBehaviour
     void Update()
     {
         Horizontal += Input.GetAxis("Horizontal")*Speed*Time.deltaTime;
+        if (!Jumping && Input.GetButtonDown("Jump"))
+            StartCoroutine(fly());
+
         Horizontal=Mathf.Clamp(Horizontal, MaxLeft, MaxRight);
-        tr.position=new Vector3(Horizontal, yOriginal, 0);
+        tr.position=new Vector3(Horizontal, yOriginal + yOffset, 0);
     }
+
+
+    public IEnumerator fly()
+    {
+        Jumping = true;
+        float d = 0;
+        while (d < JumpDuration)
+        {
+            d += Time.deltaTime;
+            yOffset = jumpCurve.Evaluate(d / JumpDuration) * JumpScale;
+            yield return null; //yield va delante de una funcion Ienumerator que devuelva null, relacionado a Coroutine, Sirve para decir que el frame acaba en esa iteracion del bucle.
+        }
+
+
+        Jumping = false;
+    }
+
 }
